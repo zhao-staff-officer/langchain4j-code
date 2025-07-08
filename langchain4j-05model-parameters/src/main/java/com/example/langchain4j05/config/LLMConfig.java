aed533b08b53c80134f7d8704ae8865979fdf95a
赵参谋 <15196332744@163.com>
2025-07-08 23:46:59 +0800

    05模型参数-监听器

--- langchain4j-05model-parameters/src/main/java/com/example/langchain4j05/config/LLMConfig.java
+++ langchain4j-05model-parameters/src/main/java/com/example/langchain4j05/config/LLMConfig.java
@@ -6,6 +6,7 @@ import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -17,9 +18,11 @@ public class LLMConfig {
                 .apiKey(System.getenv("QWEN-API-KEY"))
                 .modelName("qwen-plus")
                 .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
-                .logRequests(Boolean.TRUE)
-                .logResponses(Boolean.TRUE)
-                .listeners(List.of(new TestChatModelListener()))
+                .logRequests(Boolean.TRUE) //打印请求日志
+                .logResponses(Boolean.TRUE) //打印返回日志
+                .listeners(List.of(new TestChatModelListener())) //监听器
+                .maxRetries(2) //重试次数
+                .timeout(Duration.ofSeconds(2))//请求响应超市时间
                 .build();
     }
 
