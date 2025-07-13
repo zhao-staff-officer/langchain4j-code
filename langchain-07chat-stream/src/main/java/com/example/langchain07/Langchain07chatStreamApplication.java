256bbcf688fbf4dcb53b950cfe2ac283c8595d58
赵参谋 <15196332744@163.com>
2025-07-13 23:49:42 +0800

    新增笔记-流式输出

---
+++ langchain-07chat-stream/src/main/java/com/example/langchain07/Langchain07chatStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.langchain07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain07chatStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Langchain07chatStreamApplication.class, args);
+	}
+
+}
