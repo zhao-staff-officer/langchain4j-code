29ef979358aa6f700015a00d683536b264209209
赵参谋 <15196332744@163.com>
2025-07-15 00:00:23 +0800

    chatmemory

---
+++ langchain4j-08chat-memory/src/test/java/com/example/langchain4j08/Langchain4j08chatMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.langchain4j08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4j08chatMemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
