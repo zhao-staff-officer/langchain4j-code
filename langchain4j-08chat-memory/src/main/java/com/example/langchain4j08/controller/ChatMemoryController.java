80741e3d45c7353317dd14ce68236b71ae2d1ef2
赵参谋 <15196332744@163.com>
2025-07-16 00:25:47 +0800

    chatmemory记忆缓存

--- langchain4j-08chat-memory/src/main/java/com/example/langchain4j08/controller/ChatMemoryController.java
+++ langchain4j-08chat-memory/src/main/java/com/example/langchain4j08/controller/ChatMemoryController.java
@@ -26,11 +26,27 @@ public class ChatMemoryController {
     @GetMapping("/chatmemory/test1")
     public String chat(){
         String as1 =chatAssistant.chat("你好，我叫张三");
-        log.info("as1返回结果：",as1);
+        log.info("as1返回结果：{}",as1);
 
         String as2 =chatAssistant.chat("我的名字是什么");
-        log.info("as2返回结果：",as2);
+        log.info("as2返回结果：{}",as2);
         return "success: as1:"+as1+" as2:"+as2;
+    }
+
+    @GetMapping("/chatmemory/test2")
+    public String chat2(){
+        String as1 =chatMessageWindowAssistant.chatWithMemory(1L,"你好，我叫张三");
+        String as2 =chatMessageWindowAssistant.chatWithMemory(1L,"我的名字是什么");
+        log.info("as2返回结果：{}",as1);
+        return "success:  as2："+as2;
+    }
+
+    @GetMapping("/chatmemory/test3")
+    public String chat3(){
+        String as1 =chatTokenAssistant.chatWithMemory(1L,"你好，张三是个善良的人");
+        String as2 =chatTokenAssistant.chatWithMemory(1L,"请问张三是善良的人吗");
+        log.info("as2返回结果：{}",as1);
+        return "success:  as2："+as2;
     }
 
 }
