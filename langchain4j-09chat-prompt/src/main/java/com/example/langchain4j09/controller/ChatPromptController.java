9755c257c8b2fff0d26e4c09df457c4d92bda463
赵参谋 <15196332744@163.com>
2025-07-16 23:03:28 +0800

    chat-prompt提示词

--- langchain4j-09chat-prompt/src/main/java/com/example/langchain4j09/controller/ChatPromptController.java
+++ langchain4j-09chat-prompt/src/main/java/com/example/langchain4j09/controller/ChatPromptController.java
@@ -1,16 +1,27 @@
 package com.example.langchain4j09.controller;
 
+import com.example.langchain4j09.entity.LawPrompt;
 import com.example.langchain4j09.service.LawAssistant;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
 public class ChatPromptController {
     @Resource
     private LawAssistant lawAssistant;
+
+    @Resource
+    private ChatModel chatModel;
 
     @GetMapping("/chatprompt/test1")
     public String test1(){
@@ -27,5 +38,28 @@ public class ChatPromptController {
         System.out.println(chat4);
 
         return "success:" + "<br> \n\n chat: "+chat+"<br> \n\n chat2: "+chat2;
+    }
+
+    @GetMapping("/chatprompt/test2")
+    public String test2(){
+        LawPrompt prompt = new LawPrompt();
+        prompt.setLegal("知识产权");
+        prompt.setQuestion("什么是TRIPS协议？");
+
+        String chat = lawAssistant.chat(prompt);
+        System.out.println(chat);
+        return "success:"+"<br> \n\n chat: "+chat;
+    }
+
+    @GetMapping("/chatprompt/test3")
+    public String test3(){
+        String role = "外科医生";
+        String question = "牙疼";
+        PromptTemplate template = PromptTemplate.from("你是一个{{it}}助手,{{question}}怎么办");
+        Prompt prompt = template.apply(Map.of("it",role,"question",question));
+        UserMessage userMessage = prompt.toUserMessage();
+        ChatResponse chatResponse =  chatModel.chat(userMessage);
+        System.out.println(chatResponse.aiMessage().text());
+        return "success:"+"<br> \n\n chat: "+chatResponse.aiMessage().text();
     }
 }
--- langchain4j-09chat-prompt/src/main/java/com/example/langchain4j09/service/LawAssistant.java
+++ langchain4j-09chat-prompt/src/main/java/com/example/langchain4j09/service/LawAssistant.java
@@ -1,5 +1,6 @@
 package com.example.langchain4j09.service;
 
+import com.example.langchain4j09.entity.LawPrompt;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.V;
@@ -9,4 +10,7 @@ public interface LawAssistant {
     @SystemMessage("你是一位专业的中国法律顾问,只回答中国法律相关的问题。输出限制：对于其他领域的问题禁止回答，直接返回'抱歉，我只能回答中国法律相关的问题'")
     @UserMessage("请回答以下法律问题：{{question}},字数控制在{{length}}以内")
     String chat(@V("question")String question,@V("length")int length);
+
+    @SystemMessage("你是一位专业的中国法律顾问,只回答中国法律相关的问题。输出限制：对于其他领域的问题禁止回答，直接返回'抱歉，我只能回答中国法律相关的问题'")
+    String chat(LawPrompt lawPrompt);
 }
