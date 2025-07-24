89d1bd2ca02847f3ec3f6be0440c5675150b0181
赵参谋 <15196332744@163.com>
2025-07-24 22:57:15 +0800

    向量存储

---
+++ langchain4j-12chat-embedding/src/main/java/com/example/langchain4j12/Langchain4j12chatEmbeddingApplication.java
@@ -0,0 +1,13 @@
+package com.example.langchain4j12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4j12chatEmbeddingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Langchain4j12chatEmbeddingApplication.class, args);
+    }
+
+}
